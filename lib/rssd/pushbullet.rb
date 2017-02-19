e7c6732fa866868ef96c2b7c4eb20d52088b50fd
Hormoz K <hkheradm@gmail.com>
2017-02-19 17:34:27 -0500

    pushbullet debugging

--- lib/rssd/pushbullet.rb
+++ lib/rssd/pushbullet.rb
@@ -8,23 +8,27 @@ module RssD
 		end
 
 		def push_note(title, body)
+      puts "pushing with access token: #{@access_token}"
 			json = JSON.generate({'title' => title, 'body' => body})
 			uri = URI('https://api.pushbullet.com/v2/pushes')
 			req = Net::HTTP::Post.new(uri)
-			req['Access-Token'] = @access_token 
+			req['Access-Token'] = @access_token
 			req['Content-Type'] = 'application/json'
 			req.body = json
 
 
-			res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-			  http.request(req)
-			end
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true if uri.scheme == 'https'
+
+      res = http.start do |h|
+          h.request Net::HTTP::Get.new(uri.request_uri)
+      end
 
 			case res
 			when Net::HTTPSuccess, Net::HTTPRedirection
 			  puts "Notification sent"
 			else
-				puts "Error sending notification"
+        puts "Error sending notification: #{res.value}"
 			  res.value
 			end
 		end
