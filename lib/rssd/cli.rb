d952eb71d18b08d882cba9bb04ed09c991087bbc
Hormoz K <hkheradm@gmail.com>
2017-02-19 16:09:32 -0500

    add poller thread

--- lib/rssd/cli.rb
+++ lib/rssd/cli.rb
@@ -1,8 +1,19 @@
 require 'rssd/server'
+require 'sinatra'
 
 module RssD
   class CLI
     def self.start(*args)
+      t = Thread.new {
+        while true do
+          puts "[Thread] Fetching RSS..."
+          Server.blogs.each do |blog, url|
+            Server.rss_feeds[blog] = RSS::Parser.parse(url, false)
+          end
+          sleep 15
+        end
+      }
+      t.abort_on_exception = true
       Server.run!
     end
   end
--- lib/rssd/server.rb
+++ lib/rssd/server.rb
@@ -4,14 +4,37 @@ require 'rss'
 
 module RssD
   class Server < Sinatra::Base
+    configure do
+      @@rss_feeds = {}
+      @@blogs = {
+        sirupsen: 'http://sirupsen.com/atom.xml',
+        jvns: 'http://jvns.ca/atom.xml'
+      }
+    end
+
+    def self.rss_feeds
+      @@rss_feeds
+    end
+
+    def self.rss_feeds=(x)
+      @@rss_feeds = x
+    end
+
+    def self.blogs
+      @@blogs
+    end
+
     set :root, File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 
     get '/jvns.ca' do
-    	rss = RSS::Parser.parse('http://jvns.ca/atom.xml', false)
-    	@posts = rss.items.map do |item|
-  			Post.parse_atom(item)
-			end
-      erb :index
+      if @@rss
+        @posts = @@rss.items.map do |item|
+          Post.parse_atom(item)
+        end
+        erb :index
+      else
+        "RSS not fetched"
+      end
     end
   end
 end
