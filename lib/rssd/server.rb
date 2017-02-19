a243c242cf78a05ef753cbf899fbac8d0a824357
Hormoz K <hkheradm@gmail.com>
2017-02-19 13:53:57 -0500

    erb example

--- lib/rssd/server.rb
+++ lib/rssd/server.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
+require 'tilt/erubis'
 
 module RssD
   class Server < Sinatra::Base
     set :root, File.expand_path(File.join(File.dirname(__FILE__), '..', '..'))
 
     get '/jvns.ca' do
+      @person = "mo"
       erb :index
     end
   end
